0ea3c30d8c21276680263ad003b73c93c217174e
aryanparakh <Aryan.24BCS10336@sst.scaler.com>
2025-10-30 21:15:49 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://stream-clone-2zfi.vercel.app/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // 👈 local backend
+    : "https://stream-clone-2zfi.vercel.app/api"; // 👈 deployed backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true, // send cookies with requests
 });
